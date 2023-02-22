95cc74262003484d9d77b0d187e93ad76af81bb9
maxper4 <maxenceperion@gmail.com>
2023-02-22 11:38:51 +0100

    Added complexity for each algo

--- Algos.py
+++ Algos.py
@@ -2,12 +2,14 @@ from instanceParser import Instance, Algo, np
 from Benchmarker import benchmark
 
 # score: 15.67
+# complexity per step: O(log N) (assumed for randint) where N is the number of technicians
 class FullRandom(Algo):
     # Implementation of the algorithm, returns a random technician
     def doStep(self, site):
         return np.random.randint(0, self.instance.k)
 
 # score: 35.21
+# complexity per step: O(N) where N is the number of technicians
 class Closest(Algo):
     # Implementation of the algorithm, returns the closest technician
     def doStep(self, site):
@@ -19,6 +21,7 @@ class Closest(Algo):
         )
 
 # score: 92.48
+# complexity per step (worst case): O(N) where N is the number of technicians
 class Periodic(Algo): # Optimal quand la periode est inferieure au nombre de techniciens
     def doStep(self, site):
 
@@ -41,6 +44,7 @@ class Periodic(Algo): # Optimal quand la periode est inferieure au nombre de tec
         )
 
 # score: 67.33
+# complexity per step (worst case): O(N) where N is the number of technicians
 class PeriodicRandom(Algo):
     def doStep(self, site):
 
@@ -54,10 +58,11 @@ class PeriodicRandom(Algo):
             if tech[0] == 0 and tech[1] == 0:
                 return self.technicians.index(tech)
 
-        # Si tous les techniciens sont en deplacement, on retourne le plus proche du site
+        # Si tous les techniciens sont en deplacement, on retourne un technicien aleatoire
         return np.random.randint(0, self.instance.k)
 
 # score: 64.70
+# complexity per step (worst case): O(N) where N is the number of technicians
 class WeightedRandom(Algo):
     # Implementation of the algorithm, returns a random technician weighted by the distance to the site (the closer the more likely)
     def doStep(self, site):
@@ -75,6 +80,7 @@ class WeightedRandom(Algo):
         return np.random.choice(range(len(self.technicians)), p=p)
 
 # score: 60.31
+# complexity per step (worst case): O(N) where N is the number of technicians
 class RandomClosest(Algo):
     def doStep(self, site):
         # Dans un premier temps, on verifie si le site est deja couvert
@@ -86,6 +92,7 @@ class RandomClosest(Algo):
         return np.random.randint(0, self.instance.k)
 
 # score: 15.80
+# complexity per step: O(N^3) where N is the max between the number of technicians and the number of sites
 class MaxCoverage(Algo):
     # Try to maximize the coverage of the sites (spread the technicians as much as possible)
     def doStep(self, site):
